1c8f892c7d5df50fd1efb2cededfac705f8037ae
unknown <percy01729@gmail.com>
2026-03-13 20:39:30 +0530

    thiss

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Admissions from "../pages/Admissions";
@@ -10,26 +9,22 @@ import EventsNotices from "../pages/EventsNotices";
 import Facilities from "../pages/Facilities";
 import Gallery from "../pages/Gallery";
 import Downloads from "../pages/Downloads";
-
 import StudentLogin from "../pages/Student/StudentLogin";
 import StudentLayout from "../pages/Student/StudentLayout";
 import StudentFees from "../pages/Student/Fees";
-
 import AdminFinanceDashboard from "../pages/Admin/AdminFinanceDashboard";
 import AdminDashboard from "../pages/Admin/AdminDashboard";
 import AdminLayout from "../pages/Admin/AdminLayout";
 import ProtectedAdminLayout from "../pages/Admin/ProtectedAdminLayout";
 import AdminLogin from "../pages/Admin/AdminLogin";
-
 import StudentList from "../pages/Admin/Students/StudentList";
 import CreateStudent from "../pages/Admin/Students/CreateStudent";
 import PromoteStudents from "../pages/Admin/Students/PromoteStudents";
+import FinanceDashboard from "../pages/Admin/Finance/FinanceDashboard";
 
 export default function AppRoutes() {
   return (
     <Routes>
-
-      {/* Public Pages */}
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/admissions" element={<Admissions />} />
@@ -40,29 +35,22 @@ export default function AppRoutes() {
       <Route path="/gallery" element={<Gallery />} />
       <Route path="/downloads" element={<Downloads />} />
       <Route path="/contact" element={<Contact />} />
-
-      {/* Student Login */}
-      <Route path="/student/login" element={<StudentLogin />} />
-
-      {/* Student Panel */}
-      <Route path="/student" element={<StudentLayout />}>
-        <Route path="fees" element={<StudentFees />} />
-      </Route>
-
-      {/* Admin Login */}
+      <Route path="/student" element={<StudentLogin />} />
       <Route path="/admin/login" element={<AdminLogin />} />
 
-      {/* Admin Protected Routes */}
       <Route path="/admin" element={<ProtectedAdminLayout />}>
         <Route element={<AdminLayout />}>
           <Route path="dashboard" element={<AdminDashboard />} />
           <Route path="students" element={<StudentList />} />
           <Route path="students/create" element={<CreateStudent />} />
           <Route path="students/promote" element={<PromoteStudents />} />
+          <Route path="finance" element={<FinanceDashboard />} />
           <Route path="finance" element={<AdminFinanceDashboard />} />
         </Route>
       </Route>
-
+      <Route path="/student" element={<StudentLayout />}>
+        <Route path="fees" element={<StudentFees />} />
+      </Route>
     </Routes>
   );
 }
