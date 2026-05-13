00a3c9e7b567039e9550ed9d0ac76106a372d0e5
unknown <percy01729@gmail.com>
2026-05-13 22:24:34 +0530

    faculty fix

--- src/data/academicsSnapshot.js
+++ src/data/academicsSnapshot.js
@@ -1,7 +1,7 @@
 const academicsSnapshot = [
   { id: 1, title: "Students", value: "750+" },
   { id: 2, title: "Teachers", value: "35+" },
-  { id: 3, title: "Years of Excellence", value: "20+" },
+  { id: 3, title: "Years of Excellence", value: "25+" },
   { id: 4, title: "Awards", value: "10+" },
 ];
 export default academicsSnapshot;
